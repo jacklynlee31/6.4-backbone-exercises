bf56123cc188d08deba09effd30687fce98a7c35
Jacklyn Carroll <jacklynlee31@gmail.com>
2015-08-09 13:02:01 -0500

    Attempt 3

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ var AppTemplates = {};
 AppTemplates['form'] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "\n<form>\n    <p>First Name:</p>\n    <input type=\"text\" class=\"firstName\"> "
-    + alias3(((helper = (helper = helpers.firstName || (depth0 != null ? depth0.firstName : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"firstName","hash":{},"data":data}) : helper)))
-    + "\n    <p>Last Name:</p>\n    <input type=\"text\" class=\"lastName\"> "
-    + alias3(((helper = (helper = helpers.lastName || (depth0 != null ? depth0.lastName : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"lastName","hash":{},"data":data}) : helper)))
-    + "\n    <p>E-Mail:</p>\n    <input type=\"text\" class=\"e-mail\"> "
-    + alias3(((helper = (helper = helpers.eMail || (depth0 != null ? depth0.eMail : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"eMail","hash":{},"data":data}) : helper)))
-    + "\n    <button class=\"submit\">Submit</button>\n</form>\n";
+  return "\n<form>\n    <p>First Name:</p>\n    <input type=\"text\" id=\"first-name\"> "
+    + alias3(((helper = (helper = helpers['first-name'] || (depth0 != null ? depth0['first-name'] : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"first-name","hash":{},"data":data}) : helper)))
+    + "\n    <p>Last Name:</p>\n    <input type=\"text\" id=\"last-name\"> "
+    + alias3(((helper = (helper = helpers['last-name'] || (depth0 != null ? depth0['last-name'] : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"last-name","hash":{},"data":data}) : helper)))
+    + "\n    <p>E-Mail:</p>\n    <input type=\"text\" id=\"e-mail\"> "
+    + alias3(((helper = (helper = helpers['e-mail'] || (depth0 != null ? depth0['e-mail'] : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"e-mail","hash":{},"data":data}) : helper)))
+    + "\n    <button id=\"submit\">Submit</button>\n</form>\n";
 },"useData":true});
 // whole form
 // view
@@ -23,25 +23,29 @@ var FormView = Backbone.View.extend({
   },
 
   events: {
-    'form .submit': 'submitForm'
+    'submit form': 'createPerson'
+
+    // 'form .submit': 'submitForm'
   },
 
-  submitForm: function(ev) {
+  createPerson: function(ev) {
     ev.preventDefault();
 
-    var firstName = this.$('#firstName').val();
-    var lastName = this.$('#lastName').val();
+    var firstName = this.$('#first-name').val();
+    var lastName = this.$('#last-name').val();
     var email = this.$('#e-mail').val();
 
     this.model.save({firstName: firstName, lastName: lastName, email: email});
 
-    this.$('#firstName').val('');
-    this.$('#lastName').val('');
+    this.$('#first-name').val('');
+    this.$('#last-name').val('');
     this.$('#e-mail').val('');
+
+    this.model = new Person();
   },
 
-  onSubmit: function(ev) {
-  },
+  // onSubmit: function(ev) {
+  // },
 
   render: function() {
     var html = this.template(this.model.toJSON());
@@ -65,11 +69,13 @@ var Person = Backbone.Model.extend({
   },
 
   // url vs url root
-  urlRoot: 'http://tiny-lr.herokuapp.com/collections/jc-form'
+  url: 'http://tiny-lr.herokuapp.com/collections/jc-form'
 });
 
 // encompases 'everything'
 // 'kicks things off'
-var person = new Person();
-var form = new FormView({model: person});
+// var person = new Person();
+// var form = new FormView({model: person});
+
+var form = new FormView({model: new Person()});
 //# sourceMappingURL=app.map
